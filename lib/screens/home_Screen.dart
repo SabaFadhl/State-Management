3fa8c1f338a8a1ca4a85c398aa1c8015195fcedb
SabaFadhl <sabafadl98@gmail.com>
2025-10-27 00:11:58 +0300

    feat: home listview builder

--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:state_management/view_model/counter_vm.dart';
+import 'package:state_management/screens/cart_review.dart';
+// import 'package:state_management/view_model/counter_vm.dart';
+import 'package:state_management/view_model/productVm.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,20 +12,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ProductVm pvm = ProductVm();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          InkWell(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (ctx)=>CartReview()));
+            },
+            child: Padding(padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+            child: Badge(
+              child: Icon(Icons.shopping_cart),
+              label: Consumer<ProductVm>(builder: (ctx,p,child)=> Text("${p.cartItems.length}")),
+            ),),
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-          Provider.of<CounterVM>(context, listen: false).addOne();
+            print(pvm.cartItems.length);
+          // Provider.of<CounterVM>(context, listen: false).addOne();
           },
         ),
-      body: Center(
-        child: Consumer<CounterVM>(
-          builder: (ctx, c, child) => Text(c.counter.toString(),style: TextStyle(fontSize: 50),) ,
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: pvm.allProducts.length,
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Column(
+              children: [
+                Image.asset(pvm.allProducts[index].image),
+                ListTile(
+                  title: Text(pvm.allProducts[index].title),
+                  trailing: InkWell(
+                    onTap: () {
+                      pvm.addToCart(pvm.allProducts[index]);
+                      print("*)(*))");
+                      print(pvm.allProducts[index]);
+                    },
+                    child: Icon(Icons.add_shopping_cart),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      )
     );
   }
 }
