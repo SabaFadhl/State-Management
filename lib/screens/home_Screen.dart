cfdd94acdaae9f575ae28823f2cb92592ab8b53c
SabaFadhl <sabafadl98@gmail.com>
2025-10-27 00:25:52 +0300

    fix: error of displaying cart items

--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -1,42 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management/screens/cart_review.dart';
-// import 'package:state_management/view_model/counter_vm.dart';
 import 'package:state_management/view_model/productVm.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  ProductVm pvm = ProductVm();
-  @override
   Widget build(BuildContext context) {
+    final pvm = Provider.of<ProductVm>(context); // shared instance
+
     return Scaffold(
       appBar: AppBar(
         actions: [
           InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (ctx)=>CartReview()));
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (ctx) => CartReview()));
             },
-            child: Padding(padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-            child: Badge(
-              child: Icon(Icons.shopping_cart),
-              label: Consumer<ProductVm>(builder: (ctx,p,child)=> Text("${p.cartItems.length}")),
-            ),),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Badge(
+                child: const Icon(Icons.shopping_cart),
+                label: Text("${pvm.cartItems.length}"),
+              ),
+            ),
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print(pvm.cartItems.length);
-          // Provider.of<CounterVM>(context, listen: false).addOne();
-          },
-        ),
       body: ListView.builder(
         itemCount: pvm.allProducts.length,
         itemBuilder: (ctx, index) {
@@ -49,17 +39,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   trailing: InkWell(
                     onTap: () {
                       pvm.addToCart(pvm.allProducts[index]);
-                      print("*)(*))");
-                      print(pvm.allProducts[index]);
                     },
-                    child: Icon(Icons.add_shopping_cart),
+                    child: const Icon(Icons.add_shopping_cart),
                   ),
                 ),
               ],
             ),
           );
         },
-      )
+      ),
     );
   }
 }
